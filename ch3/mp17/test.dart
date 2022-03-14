100ea0c82e41d004a5a96025f6a9861487f0873b
Yongseok Lee <83351867+YSLee551@users.noreply.github.com>
2022-03-14 10:41:58 +0900

    Update test.dart

--- ch3/mp17/test.dart
+++ ch3/mp17/test.dart
@@ -5,7 +5,7 @@ void main() {
     if (test == "Magic WandThe magic spell worked! You killed the dragon and became a hero! :)") {
       _result(true);
     } else {
-      _result(false, ["You should choose 'Magic Wand' and print 'The magic spell worked! You killed the dragon! :)'"]);
+      _result(false, ["You should choose 'Magic Wand' and print 'The magic spell worked! You killed the dragon and became a hero! :)'"]);
     }
   } catch (e) {
     _result(false, ['Tried calling mainTest(), but received an exception: ${e.runtimeType}']);
