ff2a517dd19db29debac867b2f53398f74d6ef0f
YSLee551 <sjoms57@gmail.com>
2022-03-15 19:30:43 +0900

    mp17

--- ch3/mp17/main.dart
+++ ch3/mp17/main.dart
@@ -2,18 +2,17 @@
 // Fight a dragon with a weapon
 
 dynamic mainTest() {
-  
   var weapon = '';
-  
+
   print('There is a dragon attacking the village! Let\'s fight it!');
-	print('Your weapon is a $weapon.');
+  print('Your weapon is a $weapon.');
 
   String result;
 
   /* write your code here */
 
   print(result);
-  
+
   // -----[DO NOT write or modify from here]-----
-  return weapon + result;
+  return [weapon, result];
 }
