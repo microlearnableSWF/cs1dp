8ab6b47d1748a460f3d5a90b16593780e4af43ee
Yongseok Lee <83351867+YSLee551@users.noreply.github.com>
2022-03-15 19:47:34 +0900

    Update test.dart

--- ch3/mp17/test.dart
+++ ch3/mp17/test.dart
@@ -2,22 +2,29 @@ void main() {
   try {
     final test = mainTest();
 
-    if (test[0] == 'Lightsaber' ||
-        test[0] == 'Magic Wand' ||
-        test[0] == 'Sword' ||
-        test[0] == 'None') {
-      if (test[1] == 'The Lightsaber has no battery... You were eaten by the dragon :(' ||
-          test[1] == 'The magic spell worked! You killed the dragon and became a hero! :)' ||
-          test[1] ==
-              "The Sword cannot pierce the dragon's skin. You were eaten by a dragon :(" ||
-          test[1] == 'You cannnot fight the dragon without a weapon :(') {
-        _result(true);
-      } else {
-        _result(false, ["You have incorrect result string"]);
-      }
-    } else {
+    if (test[0] == 'Lightsaber' &&
+        test[1] ==
+            'The Lightsaber has no battery... You were eaten by the dragon :(') {
+      _result(true);
+    } else if (test[0] == 'Magic Wand' &&
+        test[1] ==
+            'The magic spell worked! You killed the dragon and became a hero! :)') {
+      _result(true);
+    } else if (test[0] == 'Sword' &&
+        test[1] ==
+            "The Sword cannot pierce the dragon's skin. You were eaten by a dragon :(") {
+      _result(true);
+    } else if (test[0] == 'None' &&
+        test[1] == 'You cannnot fight the dragon without a weapon :(') {
+      _result(true);
+    } else if (test[0] != 'Lightsaber' ||
+        test[0] != 'Magic Wand' ||
+        test[0] != 'Sword' ||
+        test[0] != 'None') {
       _result(false,
           ["You weapon should be among Lightsaber, Magic Wand, Sword or None"]);
+    } else {
+      _result(false, ["You have incorrect result string"]);
     }
   } catch (e) {
     _result(false, [
