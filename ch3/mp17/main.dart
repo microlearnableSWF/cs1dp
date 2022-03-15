3b43b975ff12285006655fac18704e0b5475ab1f
Yongseok Lee <83351867+YSLee551@users.noreply.github.com>
2022-03-15 19:36:57 +0900

    Update main.dart

--- ch3/mp17/main.dart
+++ ch3/mp17/main.dart
@@ -1,18 +1,20 @@
 // MicroProject 17
 // Fight a dragon with a weapon
 
-dynamic mainTest() {
-  var weapon = '';
-
+void main() {
+  
+	// Choices: Lightsaber, Magic Wand, Sword or None
+  var weapon = /* write your code here */
+  
   print('There is a dragon attacking the village! Let\'s fight it!');
-  print('Your weapon is a $weapon.');
+	print('Your weapon is $weapon.');
 
-  String result;
+  String result = "Default";
 
   /* write your code here */
 
   print(result);
-
+  
   // -----[DO NOT write or modify from here]-----
-  return [weapon, result];
+  // test the value of [weapon, result];
 }
