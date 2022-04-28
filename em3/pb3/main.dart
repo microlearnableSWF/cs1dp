1b7a22e1e1c386052735a8b6bdf9ce32f6a9b669
YSLee551 <sjoms57@gmail.com>
2022-04-28 17:52:28 +0900

    Exam3 pb all uploaded

---
+++ em3/pb3/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  int speed = /* write your code here */
+  bool isBreak = /* write your code here */
+  print('>>> Shift Up');
+  /* write your code here */
+  print('FINISH');
+}
+
+void pressPedal(/* write your code here */) {
+  /* write your code here */
+}
+
+void gearUpdate(/* write your code here */) {
+  /* write your code here */
+}
